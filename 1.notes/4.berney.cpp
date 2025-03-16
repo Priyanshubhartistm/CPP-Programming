911533b3955dc791000891e4ba2ffe1613176854
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-03-16 13:04:48 +0530

    berney father of cpp

---
+++ 1.notes/4.berney.cpp
@@ -0,0 +1,65 @@
+/*
+1. cpp programming father:-  Bjarne Stroustrup
+
+i) C++ 11  = Auto, lambdas, smart pointers
+sbse major version tha(jisme ye sare concepts jo phle ni the oo sb concept aye c++ ko modern banane ke liye)
+
+ii) C++ 14   = Generics
+
+iii) C++ 17  = some major updates
+
+iv) C++ 20   = some major updates
+
+v) C++ 23    = 
+
+
+vi) C++ 23    = now currently, but almost end hi h, so itna jada padhane ki jrurt ni h, itna jda.
+
+
+
+2. Why C++ = Knowldegeble language & good lang.
+system ke bare me samjhte h, isliya ham c++ padhna chahte h,
+
+
+adobe me use hota h.
+photoshop isime bana h.
+os isi me bana h.
+microsoft me use hoti h.
+fang me use hota h.
+
+3. C++ ham likhenge kaha par
+
+kch languages direct scripting type hoti h, to chal jati h, kahi v likh lete h, but
+
+C++ ek compile lang.
+means jaise hi apke pass c++ k code ata h,
+isko lgt h, software 
+
+ise software ke andr jaise hi hamri code file jati h, iske bad yaha se suru hoti h, hmri binaries, 
+
+
+binaries jaise ki exe. file executable 
+
+y jo hamra compiler h, o exe file deta h, or use exe. file ko app run krte ho isliya y portable rahta h, qki ek br binary ban gya to 
+kahi v usko run kijiye koye prblm ni ati h, fast v isliya hoti q ki jo memory management hona h, jo calculation hona h, o sb ho chuka h, 
+
+ab to sidha jo s\w o run krta h, to yhi hamri binaries k process rhta h, to ab kahi n kahi 
+
+
+to ab binary kha se milegi to apko bata de ki binaries depend karega ki ap linux, mac, window, online/offline chize execute krni h, to alg alg tarike h. 
+
+apko bht sare library compilers h like:- g++,mingw,
+
+online compiler :- replit
+y sb kise or ke server pr run krte h, hm to yaha pr milti h, kch restractions ki ap kya kr skte kya ni kr skte ho, 
+
+kn si library ham access hone denge kn si nhi hone denge specially, ap jab time segment ko padhte ho kise v compiler me kise v language me ,
+
+o time hota h, standard server k time ; app yaha pr confused ho jate ho ki maine to y time ni bola 
+
+or ap iske andr files ko ni padh skte ho qki , server thodi n allow karega ki ap, koye v file upload/download/read/delete o ni krta h, chota s ek prblm h. 
+
+
+
+
+*/
