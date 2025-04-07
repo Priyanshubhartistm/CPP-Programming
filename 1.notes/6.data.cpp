56d071cc85754658f0344ca4dbf735632116e2f5
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-04-07 22:39:36 +0530

    data

---
+++ 1.notes/6.data.cpp
@@ -0,0 +1,28 @@
+/*
+
+1. DATA
+har program ke andr y jo v app software banyenge uska main reason hota h, data 
+
+2. ab data se related ham do hi kam krte h.
+
+i) STORE     = y to store karwate ho y
+ii) PROCESS  = process krate fir display.
+
+y dono line jo h, hona hi chahya.
+
+iii) DISPLAY = or iske bad y jo display wala kam h, y to hona hi hona h, ab chahe display ho y read ho y, jo v app bolna chahe bol lijiye. 
+
+but y major kam h , data k jo app karwana chaha rahe h, 
+
+y jo display wala kam h, isme ham jada intersted h, q ki data ko display karwan hi karwan h, isme koye option h, hi ni apke pass . 
+
+** user hamse data read karana chahega chahe oo web ke through ho y command line ke through y fir console ke through.
+
+
+to data ka jo ultimate goal h, o yahi h, but use pahle v data k ek goal h, directly aap data display i karwate ho, 
+
+
+
+
+
+*/
