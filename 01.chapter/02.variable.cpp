e57e61a27b82da0aa9e6f67d5433c677d3a1624c
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-04-07 22:42:20 +0530

    var

--- 01.chapter/02.variable.cpp
+++ 01.chapter/02.variable.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main () {
 
-    cout<<"hello p";
+    int score;
+    score = 110;
 
+    int balance = 300;
+    cout<<"hello priyanshu 1" <<endl;
+    
+    cout<<"hello priyanshu 2" <<endl;
+    cout<<"hello priyanshu 3" <<endl;
     return 0;
-                
+
     
 }
