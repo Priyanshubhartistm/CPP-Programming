332995701149da86bb20b3561c2cfca5b99947b8
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-04-18 20:36:12 +0530

    data type

---
+++ 1.notes/7.Data types.cpp
@@ -0,0 +1,47 @@
+/*
+
+Data types = manyly 3 types
+
+1. Primitive          = By default , like:- sabse basic unit jo memory me store hoti h, use premetive bola jata h.
+
+2. Derived            = primitive upr hi ham kch or feature add karna chahte h, y kch or functionality dena chahate h, to usko ham derived bolte h, hoga to primitive hi but uska thoda se enhance roop dikhega.
+
+3. User-defined       = jo ham apni tarf se defined krte h, like:- from jo ek structure h, jo apke data ko kaise rkhta h, o batata h.
+
+1. Primitive = 6 types
+
+i) int
+ii) float
+iii) double
+iv) char        = i want to store character value. like:- ch , a 
+v) boolean      = i want to store boolean value, true and false.
+vi) void        = that's mean NULL Nothing.But some language, in void values are changes , so dont compare.
+
+2. Dervied = 4 types
+
+i) Array = collection of similar data types.
+ii) Function = 
+iii) Pointer = thats mean memory me jaha pe v y store ho raha h, uska sidha memory k address hi de do.
+
+to use memory ke address ko reference krne ko pointer bolte h, to pointer to o chiz h, jisko point kar diya.
+
+iv) Reference = kayi bar apko use pointer se jo value milti h, usko bolte h, reference.
+
+3. User-defined = 
+
+i) Structure =                 
+ii) Union
+iii) Enum
+iv) Class
+v) Typedef
+
+
+
+
+
+
+
+
+
+
+*/
