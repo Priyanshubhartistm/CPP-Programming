4d223f8e6e9ad340288c934c392bb5c9d96b5699
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-04-20 18:37:52 +0530

    data type

---
+++ 01.chapter/04.Data types.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    int tealeaves = 50;
+    float watertemperature = 85.5;
+    double priceoftea = 109.99;
+    char teagrade = 'A';
+    bool isTeaReady = 5;
+
+    //cout << isTeaReady << endl;
+
+    return 0;
+
+
+
+}
